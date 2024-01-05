0e171e8da9c246c60c98c372fbf15936382f100a
Kumar@123 <Ajay@extensioncrm.com>
2024-01-05 17:09:30 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<void> _authenticate() async {
     final clientId = '8f4562b21a';
-    final redirectUri = 'https://frappelogin.vercel.app/oauth2/callback';
+    final redirectUri = 'https://frappelogin.vercel.app/';
     final authUrl =
         "https://demo.extensionerp.com/api/method/frappe.integrations.oauth2.authorize?client_id=$clientId&response_type=code&grant_type=Authorization Code&redirect_uri=$redirectUri";
     try {

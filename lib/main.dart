3b35259ed8d1e2344a6a6739dcbb71c80faf299c
Kumar@123 <Ajay@extensioncrm.com>
2024-01-05 12:59:11 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return const MaterialApp(home: HomeScreen());
   }
 }
 

712a541f81cdd015ab08838fe2660fcb67354520
Kumar@123 <Ajay@extensioncrm.com>
2024-01-05 12:50:45 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
 import 'package:flutter/material.dart';
-import 'dart:js';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: HomeScreen());
